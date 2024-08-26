f405c5b523d935e64a84790f638f80130ab74542
Dipanshu Mishra <dipanshu.a.mishra06@gmail.com>
2024-08-26 17:17:37 +0530

    Made the login page responsive.

--- frontend/src/Pages/login.jsx
+++ frontend/src/Pages/login.jsx
@@ -36,20 +36,20 @@ export default function Login() {
   return (
     <>
       {loading && <Spinner />}
-      <div className='flex justify-around h-screen w-full'>
+      <div className='flex justify-around h-screen w-full md:flex-col sm:flex-col'>
 
-        <div className='w-1/2 min-h-fit overflow-hidden busimg'>
+        <div className='w-1/2 md:w-full sm:w-full h-screen overflow-hidden busimg md:max-h-min sm:max-h-min'>
         </div>
 
-        <div className='w-1/2 h-full bg-slate-100 flex items-center justify-center flex-col space-y-8 login'>
+        <div className='w-1/2 sm:w-full md:w-full bg-slate-100 flex items-center justify-center p-4 space-y-8 flex-col login md:space-y-5 sm:space-y-3 overflow-auto'>
 
-          <div className='flex flex-col items-center justify-center'>
-            <img src={PU} alt="No" className='h-[5rem]' />
-            <h3 className='font-bold text-center text-4xl mt-4'>Transport Management System</h3>
-            <h3 className='tracking-wide'>User Login</h3>
+          <div className='flex flex-col items-center justify-center md:mt-[6rem] sm:mt-20'>
+            <img src={PU} alt="No" className='h-[5rem] md:h-[3rem] sm:h-[2rem]' />
+            <h3 className='font-bold text-center text-4xl mt-4 md:text-2xl sm:text-xl'>Transport Management System</h3>
+            <h3 className='tracking-wide md:text-xl sm:text-[1rem]'>User Login</h3>
           </div>
 
-          <div className='flex flex-col w-[20rem] h-[15rem] space-y-5 bg-slate-200 rounded-3xl items-center justify-center'>
+          <div className='flex flex-col w-[20rem] h-[15rem] space-y-5 bg-slate-200 rounded-3xl items-center justify-center md:space-y-3 sm:space-y-2 p-2'>
             <input type="email" placeholder='User Id' className='bg-slate-100 rounded-lg w-[80%] p-2' value={user_id} onChange={(e) => setUserID(e.target.value)} required />
             <input type="password" placeholder='Password' className='bg-slate-100 rounded-lg w-[80%] p-2' value={password} onChange={(e) => setPassword(e.target.value)} required />
             <button className='bg-green-500 py-2 px-3 rounded-lg hover:bg-green-600 ease-in-out transition' onClick={() => {
@@ -60,7 +60,6 @@ export default function Login() {
 
           <div>
             <button className='bg-green-500 py-2 px-3 my-2 mx-2 rounded-lg hover:bg-green-600 ease-in-out transition '><Link to="/admin-login" className="text-black no-underline hover:text-white">Admin Login</Link></button>
-            {/* <button className='bg-blue-500 py-2 px-3 my-2 mx-2 rounded-lg hover:bg-blue-700 ease-in-out transition '><Link to="/forgot-password" className="text-white no-underline hover:text-white">Forgot Password</Link></button> */}
 
           </div>
 
